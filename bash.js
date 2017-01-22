0084f3a09a4fb66985739e4834af620faaf06144
Evan Williams <evan.williams@moat.com>
2017-01-22 16:55:38 -0500

    added piping...idk how but i did it

--- bash.js
+++ bash.js
@@ -1,10 +1,14 @@
 var cmds = require('./cmds');
 process.stdout.write('prompt > ');
-
+var nextCmd;
 let cbOutFnc = function(err, result) {
     if (err) {
         process.stdout.write(err.toString());
     } else {
+        if (nextCmd){
+            inputData(nextCmd, result);
+            return null;
+        }
         process.stdout.write(result);
     }
     process.stdout.write('\nprompt > ');
@@ -14,14 +18,20 @@ let envVars = {
         $PATH: () => __dirname
     }
     // The stdin 'data' event fires after a user types in a line
-process.stdin.on('data', function(data) {
+    let inputData = function(data, processedData) {
     var input = data.toString().trim(); // remove the newline
-    var params = input.split(' ');
+    nextCmd = input.split('|');
+
+    var params = nextCmd.shift().split(' ');
+    nextCmd = nextCmd.join('|');
     var cmd = params.shift();
     params = params.map((param) => envVars[param] ? envVars[param]() : param);
+    params = processedData ? [processedData].concat(params) : params;
     if (cmds[cmd]) {
-        cmds[cmd](cbOutFnc, params);
+        cmds[cmd](cbOutFnc,params);
     } else {
         cbOutFnc(null, 'You typed: ' + input);
     }
-});
+}
+process.stdin.on('data', inputData);
+
